9876a93945bf152e0fab416b69c3d0cbc5a91c99
Luis Miguel Montes <54339559+luismiguel010@users.noreply.github.com>
2023-08-03 13:51:57 -0500

    Update chatgpt

--- chatgpt.py
+++ chatgpt.py
@@ -1,16 +1,16 @@
-import openai
+import openai  # pip install openai
 
-openai.api_key = "{{API_KEY_OPENAI}}"
+openai.api_key = {{Llave}}
+context = {"role": "system",
+               "content": "Eres un asistente muy útil."}
+messages = [context]
+print("Hola jefe. ¿En qué te puedo ayudar?")
 
 while True:
-
-    prompt = input("\nIntroduce una pregunta: ")
-
-    if prompt == "exit":
-      break
-
-    completion = openai.Completion.create(engine="text-davinci-003",
-                          prompt=prompt,
-                          max_tokens=2048)
-
-    print(completion.choices[0].text)
+    pregunta = input("\n＞ ")
+    if pregunta == "Chao":
+        print("¡Hasta luego, jefe!")
+        break
+    messages.append({"role": "user", "content": pregunta})
+    respuesta = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+    print(respuesta.choices[0].message.content)
